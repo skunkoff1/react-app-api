397b6ffdf4b3e6e7bc1096de71c8d2b1c8e13ffe
DIDRICH Damien <89045920+skunkoff1@users.noreply.github.com>
2023-01-20 14:47:53 +0100

    update register controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -8,7 +8,6 @@ const login = (req, res) => {
 
     User.findOne({ username : username})
         .then((user) => {
-            console.log(user);
             if(pwd != user.password) {
                 return res.status(401).json({'message' : "utilisateur et/ou mot de passe incorrect"});
             }
@@ -22,25 +21,46 @@ const login = (req, res) => {
             
         })
         .catch((err) => {
-            res.status(500).json({ 'error': err });
+            return res.status(401).json({'message' : "utilisateur et/ou mot de passe incorrect"});
         })
 }
 
 const register = (req, res) => {
+
+    const USER_REGEX = /^[A-z][A-z0-9-_]{3,23}$/;
+    const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
+
+    if (!USER_REGEX.test(req.body.username)) {
+        return res.status(400).json({ 'error': 'format du nom d\'utilisateur incorrect' });
+    }
+    if (!PWD_REGEX.test(req.body.pwd)) {
+        return res.status(400).json({ 'error': 'format du mot de passe incorrect' });
+    }
     
-    const newUserRecord = new User({
-        username: req.body.username,
-        password: req.body.pwd,
-    })
-    
-    newUserRecord.save((err, result) => {
-        if(!err) {
-            return res.status(201).send(result);
-        }
-        else {
-            return res.status(400).json({"error" : err});
-        }
-    })
+    User.findOne({ username : req.body.username})
+        .then((user) => {
+           if(user) {
+                return res.status(401).json({'message' : "nom d'utilisateur déjà utilisé"});
+           }
+           else {
+                const newUserRecord = new User({
+                    username: req.body.username,
+                    password: req.body.pwd,
+                });
+
+                newUserRecord.save((err, result) => {
+                    if(!err) {
+                        return res.status(201).send(result);
+                    }
+                    else {
+                        return res.status(400).json({"error" : err});
+                    }
+                })
+           }
+        })
+        .catch((err) => {
+            return res.status(400).json({'erreur' : err});
+        })
 }
 
 module.exports = { login, register};
